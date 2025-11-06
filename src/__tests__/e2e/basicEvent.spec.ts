602de048c30d862f59044b63d31553ea00414a69
jthw1005 <hjt105@gmail.com>
2025-11-07 00:57:31 +0900

    feat: e2e test for updating event

--- src/__tests__/e2e/basicEvent.spec.ts
+++ src/__tests__/e2e/basicEvent.spec.ts
@@ -10,28 +10,27 @@ const __dirname = path.dirname(__filename);
 const scheduleFilePath = path.join(__dirname, '../../__mocks__/response/realEvents.json');
 let originalData: string;
 
-test.describe.serial('기본 일정 관리 E2E 테스트', () => {
-  test.beforeAll(async () => {
-    originalData = fs.readFileSync(scheduleFilePath, 'utf-8');
-    console.log('📦 원본 데이터 백업 완료');
-  });
+test.beforeAll(async () => {
+  originalData = fs.readFileSync(scheduleFilePath, 'utf-8');
+  console.log('📦 원본 데이터 백업 완료');
+});
 
-  test.afterAll(async () => {
-    fs.writeFileSync(scheduleFilePath, originalData);
-    console.log('♻️ 원본 데이터로 복원 완료');
-  });
+test.afterAll(async () => {
+  fs.writeFileSync(scheduleFilePath, originalData);
+  console.log('♻️ 원본 데이터로 복원 완료');
+});
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+  console.log('✅ 테스트 페이지 이동 완료');
+});
+
+test.describe('기본 일정 관리 E2E 테스트', () => {
   test('기존 일정을 확인할 수 있다.', async ({ page }) => {
-    await page.goto('/');
-    await expect(page.getByRole('alert').nth(0)).toContainText('일정 로딩 완료!');
-
     await expect(page.getByTestId('event-list')).toContainText('테니스 레슨');
   });
 
   test('새로운 일정을 생성할 수 있다.', async ({ page }) => {
-    await page.goto('/');
-    await expect(page.getByRole('alert').nth(0)).toContainText('일정 로딩 완료!');
-
     await page.getByRole('textbox', { name: '제목' }).click();
     await page.getByRole('textbox', { name: '제목' }).fill('시훈이 만나기');
 
@@ -60,5 +59,30 @@ test.describe.serial('기본 일정 관리 E2E 테스트', () => {
     await expect(page.getByRole('alert').nth(0)).toContainText('일정이 추가되었습니다');
 
     await expect(page.getByTestId('event-list')).toContainText('시훈이 만나기');
+  });
+
+  test('기존 일정을 수정할 수 있다.', async ({ page }) => {
+    await page
+      .getByTestId('event-list')
+      .locator('div')
+      .filter({ hasText: '테니스 레슨' })
+      .getByRole('button', { name: 'Edit event' })
+      .first()
+      .click();
+
+    await page.getByRole('textbox', { name: '제목' }).click();
+    await page.getByRole('textbox', { name: '제목' }).fill('테니스 레슨 시간 변경');
+
+    await page.getByRole('textbox', { name: '시작 시간' }).click();
+    await page.getByRole('textbox', { name: '시작 시간' }).fill('18:00');
+
+    await page.getByRole('textbox', { name: '종료 시간' }).click();
+    await page.getByRole('textbox', { name: '종료 시간' }).fill('21:00');
+
+    await page.getByTestId('event-submit-button').click();
+
+    await expect(page.getByRole('alert').nth(0)).toContainText('일정이 수정되었습니다');
+
+    await expect(page.getByTestId('event-list')).toContainText('테니스 레슨 시간 변경');
   });
 });
