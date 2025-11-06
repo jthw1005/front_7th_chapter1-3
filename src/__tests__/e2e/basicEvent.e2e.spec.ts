24ca93cea50b8253f236c41adc4d2a760a5527af
jthw1005 <hjt105@gmail.com>
2025-11-07 03:51:08 +0900

    fix: initiate mock db

--- src/__tests__/e2e/basicEvent.e2e.spec.ts
+++ src/__tests__/e2e/basicEvent.e2e.spec.ts
@@ -5,27 +5,27 @@ test.describe('기본 일정 관리 E2E 테스트', () => {
     await page.goto('/');
   });
 
-  test.skip('기존 일정을 확인할 수 있다.', async ({ page }) => {
-    await expect(page.getByTestId('event-list')).toContainText('반복 일정');
+  test('기존 일정을 확인할 수 있다.', async ({ page }) => {
+    await expect(page.getByTestId('event-list')).toContainText('팀 회의');
   });
 
   test('새로운 일정을 생성할 수 있다.', async ({ page }) => {
     await page.getByRole('textbox', { name: '제목' }).click();
-    await page.getByRole('textbox', { name: '제목' }).fill('시훈이 만나기');
+    await page.getByRole('textbox', { name: '제목' }).fill('영화보기');
 
-    await page.getByRole('textbox', { name: '날짜' }).fill('2025-11-02');
+    await page.getByRole('textbox', { name: '날짜' }).fill('2025-11-04');
 
     await page.getByRole('textbox', { name: '시작 시간' }).click();
-    await page.getByRole('textbox', { name: '시작 시간' }).fill('13:00');
+    await page.getByRole('textbox', { name: '시작 시간' }).fill('18:00');
 
     await page.getByRole('textbox', { name: '종료 시간' }).click();
-    await page.getByRole('textbox', { name: '종료 시간' }).fill('14:00');
+    await page.getByRole('textbox', { name: '종료 시간' }).fill('20:00');
 
     await page.getByRole('textbox', { name: '설명' }).click();
-    await page.getByRole('textbox', { name: '설명' }).fill('새로운 일정 생성 테스트');
+    await page.getByRole('textbox', { name: '설명' }).fill('다크나이트');
 
     await page.getByRole('textbox', { name: '위치' }).click();
-    await page.getByRole('textbox', { name: '위치' }).fill('미정');
+    await page.getByRole('textbox', { name: '위치' }).fill('여의도');
 
     await page.getByRole('combobox', { name: '업무' }).click();
     await page.getByRole('option', { name: '개인-option' }).click();
@@ -37,20 +37,20 @@ test.describe('기본 일정 관리 E2E 테스트', () => {
 
     await expect(page.getByRole('alert').last()).toContainText('일정이 추가되었습니다');
 
-    await expect(page.getByTestId('event-list')).toContainText('시훈이 만나기');
+    await expect(page.getByTestId('event-list')).toContainText('영화보기');
   });
 
-  test.skip('기존 일정을 수정할 수 있다.', async ({ page }) => {
+  test('기존 일정을 수정할 수 있다.', async ({ page }) => {
     await page
       .getByTestId('event-list')
       .locator('div')
-      .filter({ hasText: '테니스 레슨' })
+      .filter({ hasText: '점심 약속' })
       .getByRole('button', { name: 'Edit event' })
       .first()
       .click();
 
     await page.getByRole('textbox', { name: '제목' }).click();
-    await page.getByRole('textbox', { name: '제목' }).fill('테니스 레슨 시간 변경');
+    await page.getByRole('textbox', { name: '제목' }).fill('저녁 약속');
 
     await page.getByRole('textbox', { name: '시작 시간' }).click();
     await page.getByRole('textbox', { name: '시작 시간' }).fill('18:00');
@@ -62,20 +62,20 @@ test.describe('기본 일정 관리 E2E 테스트', () => {
 
     await expect(page.getByRole('alert').last()).toContainText('일정이 수정되었습니다');
 
-    await expect(page.getByTestId('event-list')).toContainText('테니스 레슨 시간 변경');
+    await expect(page.getByTestId('event-list')).toContainText('저녁 약속');
   });
 
-  test.skip('기존 일정을 삭제할 수 있다.', async ({ page }) => {
+  test('기존 일정을 삭제할 수 있다.', async ({ page }) => {
     await page
       .getByTestId('event-list')
       .locator('div')
-      .filter({ hasText: '삭제될 이벤트' })
+      .filter({ hasText: '생일 파티' })
       .getByRole('button', { name: 'Delete event' })
       .first()
       .click();
 
     await expect(page.getByRole('alert').last()).toContainText('일정이 삭제되었습니다');
 
-    await expect(page.getByTestId('event-list')).not.toContainText('삭제될 이벤트');
+    await expect(page.getByTestId('event-list')).not.toContainText('생일 파티');
   });
 });
--- src/__tests__/e2e/global.teardown.ts
+++ src/__tests__/e2e/global.teardown.ts
@@ -1,9 +1,17 @@
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import { test as teardown } from '@playwright/test';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 teardown('DB 초기화', async () => {
   console.log('🗑️ Reset Test Database...');
 
-  fs.writeFileSync(`${__dirname}/../__mocks__/response/e2e.json`, JSON.stringify({ events: [] }));
+  fs.writeFileSync(
+    `${__dirname}/../../__mocks__/response/e2e.json`,
+    JSON.stringify({ events: [] })
+  );
 });
