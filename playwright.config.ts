3b8325667cc02a2b236213915a8707e7622fe1cd
jthw1005 <hjt105@gmail.com>
2025-11-07 00:37:54 +0900

    chore: modify playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,10 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    headless: true,
+    baseURL: 'http://localhost:5173',
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
