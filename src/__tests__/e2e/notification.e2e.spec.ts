157cd15146d108a109cea60e58b6125d3b41892b
jthw1005 <hjt105@gmail.com>
2025-11-07 15:34:55 +0900

    chore: fix mock events

--- src/__tests__/e2e/notification.e2e.spec.ts
+++ src/__tests__/e2e/notification.e2e.spec.ts
@@ -5,8 +5,8 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('테스트를 위한 데이터 시딩', async ({ page }) => {
-  await page.getByRole('textbox', { name: '제목' }).fill('연극보기');
-  await page.getByRole('textbox', { name: '날짜' }).fill('2025-11-06');
+  await page.getByRole('textbox', { name: '제목' }).fill('스터디 하기');
+  await page.getByRole('textbox', { name: '날짜' }).fill('2025-11-18');
   await page.getByRole('textbox', { name: '시작 시간' }).fill('18:00');
   await page.getByRole('textbox', { name: '종료 시간' }).fill('20:00');
   await page.getByTestId('event-submit-button').click();
@@ -16,9 +16,9 @@ test('테스트를 위한 데이터 시딩', async ({ page }) => {
 });
 
 test('알림이 울리는 경우', async ({ page }) => {
-  await expect(page.getByText('연극보기').first()).toBeVisible();
-  await expect(page.getByText('10분 후 연극보기 일정이 시작됩니다.')).not.toBeVisible();
+  await expect(page.getByText('스터디 하기').first()).toBeVisible();
+  await expect(page.getByText('10분 후 스터디 하기 일정이 시작됩니다.')).not.toBeVisible();
 
-  page.clock.setFixedTime(new Date('2025-11-06T17:50'));
-  await expect(page.getByText('10분 후 연극보기 일정이 시작됩니다.')).toBeVisible();
+  page.clock.setFixedTime(new Date('2025-11-18T17:50'));
+  await expect(page.getByText('10분 후 스터디 하기 일정이 시작됩니다.')).toBeVisible();
 });
--- src/__tests__/e2e/searchAndFilter.e2e.spec.ts
+++ src/__tests__/e2e/searchAndFilter.e2e.spec.ts
@@ -7,27 +7,28 @@ test.describe('검색 및 필터 E2E 테스트', () => {
 
   test('테스트를 위한 데이터 시딩', async ({ page }) => {
     await page.getByRole('textbox', { name: '제목' }).fill('영화보기');
+    await page.getByRole('textbox', { name: '날짜' }).fill('2025-11-03');
+    await page.getByRole('textbox', { name: '시작 시간' }).fill('18:00');
+    await page.getByRole('textbox', { name: '종료 시간' }).fill('20:00');
+    await page.getByTestId('event-submit-button').click();
+    await expect(page.getByRole('alert').last()).toContainText('일정이 추가되었습니다');
+    await expect(page.getByTestId('event-list')).toContainText('영화보기');
+
+    await page.getByRole('textbox', { name: '제목' }).fill('뮤지컬보기');
     await page.getByRole('textbox', { name: '날짜' }).fill('2025-11-04');
     await page.getByRole('textbox', { name: '시작 시간' }).fill('18:00');
     await page.getByRole('textbox', { name: '종료 시간' }).fill('20:00');
     await page.getByTestId('event-submit-button').click();
     await expect(page.getByRole('alert').last()).toContainText('일정이 추가되었습니다');
+    await expect(page.getByTestId('event-list')).toContainText('뮤지컬보기');
 
-    await page.getByRole('textbox', { name: '제목' }).fill('뮤지컬보기');
+    await page.getByRole('textbox', { name: '제목' }).fill('연극보기');
     await page.getByRole('textbox', { name: '날짜' }).fill('2025-11-05');
     await page.getByRole('textbox', { name: '시작 시간' }).fill('18:00');
     await page.getByRole('textbox', { name: '종료 시간' }).fill('20:00');
     await page.getByTestId('event-submit-button').click();
     await expect(page.getByRole('alert').last()).toContainText('일정이 추가되었습니다');
-
-    await page.getByRole('textbox', { name: '제목' }).fill('연극보기');
-    await page.getByRole('textbox', { name: '날짜' }).fill('2025-11-06');
-    await page.getByRole('textbox', { name: '시작 시간' }).fill('18:00');
-    await page.getByRole('textbox', { name: '종료 시간' }).fill('20:00');
-    await page.getByTestId('event-submit-button').click();
-    await expect(page.getByRole('alert').last()).toContainText('일정이 추가되었습니다');
-
-    await page.waitForTimeout(1000);
+    await expect(page.getByTestId('event-list')).toContainText('연극보기');
   });
 
   test('검색어가 비어있는 경우 모든 일정을 확인할 수 있다.', async ({ page }) => {
